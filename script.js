8f1649dc0ca4f94139cbfad5c0d5950b8a690a26
Rishikesh Mane <139518215+RishiMane03@users.noreply.github.com>
2023-09-28 16:06:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+function addMessage() {
+    var body = document.body;
+    body.innerHTML = "DOM load success";
+}
+
+// Add the message when the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", addMessage);
